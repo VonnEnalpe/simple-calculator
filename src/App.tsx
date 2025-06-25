72ff2ec9523f55ef7c563c20e403b193d3e63bbd
VonnEnalpe <vonnenalpe@gmail.com>
2025-06-26 00:22:03 +0800

    added some operators and digits functionality

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,10 @@ interface Button {
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [previousValue, setPreviousValue] = useState("");
-  const [operatorValue, setoperatorValue] = useState("");
+  const [operatorValue, setOperatorValue] = useState("");
+  // const [displayCurrentValue, setDisplayCurrentValueState] = useState("");
 
-  
-
-  const buttons = [
+  const buttonValues = [
     { label: "CE", action: "clearEntry" },
     { label: "C", action: "clearAll" },
     { label: "←", action: "backspace" },
@@ -44,62 +43,94 @@ function App() {
     { label: "=", action: "equals" },
   ];
 
-  const handleClick = (label: string, action: string) =>{
-    switch(action){
+  const handleClick = (label: string, action: string) => {
+    switch (action) {
       case "digit":
         setInputValue((previousInput) => previousInput + label);
         break;
 
       case "decimal":
-        if(inputValue === ''){
-          setInputValue('0.')
-        } else if (!inputValue.includes('.')){
-          setInputValue((previousInput) => previousInput + '.');
+        if (inputValue === "") {
+          setInputValue("0.");
+        } else if (!inputValue.includes(".")) {
+          setInputValue((previousInput) => previousInput + ".");
         }
         break;
 
       case "operator":
         if (inputValue) {
           setPreviousValue(inputValue);
-          setoperatorValue(label);
-          setInputValue('');
+          setOperatorValue(label);
+          setInputValue("");
         }
         break;
 
       case "clearEntry":
-        setInputValue('');
+        setInputValue("");
         break;
-      
+
       case "clearAll":
-        setInputValue('');
+        setInputValue("");
+        setPreviousValue("");
+        setOperatorValue("");
+        break;
+
+      case "equals":
+        if (!inputValue || !previousValue || !operatorValue) return;
+        const convertPreviousValue = parseFloat(previousValue);
+        const convertInputValue = parseFloat(inputValue);
+        let result = 0;
+
+        switch (operatorValue) {
+          case "+":
+            result = convertPreviousValue + convertInputValue;
+            break;
+          case "-":
+            result = convertPreviousValue - convertInputValue;
+            break;
+          case "x":
+            result = convertPreviousValue * convertInputValue;
+            break;
+          case "÷":
+            result = convertPreviousValue / convertInputValue;
+            break;
+        }
+
+        setPreviousValue("");
+        // setDisplayCurrentValueState(`${result} ${label}`);
+        setInputValue(JSON.stringify(result));
+        setOperatorValue("");
         break;
     }
-  }
+  };
 
   const onButtonClick = (btn: Button) => {
     handleClick(btn.label, btn.action);
-  }
+  };
 
-  const getColorScheme = (label: string) =>{
-    if (["+", "−", "×", "÷"].includes(label)) return "blue";
-    if (label === "=") return "green";
-    if (["C", "CE"].includes(label)) return "red";
-    return "gray";
-  }
+  // const getColorScheme = (label: string) =>{
+  //   if (["+", "−", "×", "÷"].includes(label)) return "blue";
+  //   if (label === "=") return "green";
+  //   if (["C", "CE"].includes(label)) return "red";
+  //   return "gray";
+  // }
 
   return (
     <>
       <Container>
+        {/* <Text fontSize="sm" color="gray.500">
+          {displayCurrentValue}
+        </Text> */}
         <Text>{inputValue || "0"}</Text>
-        <SimpleGrid columns={4}>
-          {buttons.flat().map((data, index) => (
+        <SimpleGrid columns={5}>
+          {buttonValues.flat().map((data, index) => (
             <Button
               key={index}
               onClick={() => onButtonClick(data)}
               children={data.label}
               size="lg"
               variant="solid"
-              colorScheme={getColorScheme(data.label)}
+              // colorScheme={getColorScheme(data.label)}
             />
           ))}
         </SimpleGrid>
